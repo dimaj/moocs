f933f052efc29a38dbb103d5963c8572219667a0
manbropig <Jamie.Tahirkheli@gmail.com>
2013-04-14 17:53:29 -0700

    Update udacity.php
    Getting started on Udacity.com scraper based off Dmitry's work. 
    -Jamie

--- scrapers/udacity.php
+++ scrapers/udacity.php
@@ -5,15 +5,47 @@
  */
 class Udacity {
 	function __construct() {
-		$this->url = "https:/www.udacity.com";
+		$this->url = "https://www.udacity.com/courses";
 		$this->scraperName = "Udacity";
 		print "Class Udacity has been created.\n";
 	}
 	
 	function scrape($classes) {
-		print "In Udacity's scrape method.\n";
-		$classes = array("Udacity 1", "Udacity 2");
+		$site = file_get_html($this->url);
+		
+		$classes = $this->getClasses($site);
 		return true;
 	}
+        
+        private function getClasses($site) {
+		$classes = array();
+		// loop through classes on the site
+		foreach ($site->find('li data-ng-show="isCourseShown') as $class) 
+                    {
+                        
+			$basic = $this->getBasicClassInfo($class);
+
+			$detailed = $this->getDetailedDescription($basic["link"]);
+			$classInfo = array_merge($basic, $detailed);
+			
+			array_push($classes, $classInfo);
+                    }
+		
+		return $classes;
+	}
+        
+        private function getBasicClassInfo($class) {
+                $link  = $class->find('style a', 0)->getAttribute('href');
+            
+		$shortDesc = $class->find('div class="crs-li-text"', 0)->text();
+		
+		$retVal = array(
+			"link" => trim($link),
+			"shortDesc" => trim($shortDesc),
+			"site" => "Udacity"
+		);
+		
+		return $retVal;
+        }
 }
 ?>
